8701107212b5b61f9185f6ea8338246d64b5aca8
Sontos Sharma <sontossharma926@gmail.com>
2024-03-31 23:59:51 +0600

    livetest

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/menu">Menu</router-link>
+    </nav>
+    <main>
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<script>
+import router from './router';
+
+export default {
+  router,
+};
+</script>
